af4e8806130ef1c598e06786f5b10040131983c2
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-07-26 15:07:04 -0600

    added seeds and faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,15 @@
 10.times do User.create(username: Faker::Internet.username , password: Faker::Internet.password ,  email: Faker::Internet.email ,  admin: false)
 end
 
-5.times do User.create(username: Faker::Internet.username , password: Faker::Internet.password , email: Faker::Internet.email , admin: true)
+5.times do User.create(username: Faker::Internet.username, password: Faker::Internet.password, email: Faker::Internet.email, admin: true)
 end
 
-20.times do Gig.create(type: "rehearsal", status: "pending", street_address: Faker::Address.street_address, secondary_address: Faker::Address.secondary_address, city: Faker::Address.city, state: Faker::Address.state, zip: Faker::Address.zip)
+20.times do Gig.create(type: "rehearsal", status: "pending", street_address: Faker::Address.street_address, secondary_address: Faker::Address.secondary_address, city: Faker::Address.city, state: Faker::Address.state, zip: Faker::Address.zip, start_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, end_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+
+10.times do Invoice.create(amount: Faker::Number.decimal(l_digits: 2), description: Faker::Lorem.sentences)
+
+10.times do Song.create(title: Faker::Music::Phish.song, artist: Faker::Music.band)
+
+10.times do SetList.create
+10.times do Calendar.create
+
