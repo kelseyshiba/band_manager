12b1f4241bfffc66defd905bfcf189c5b3398824
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-07-27 11:21:20 -0600

    added simple calendar

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -3,9 +3,14 @@ class Gig < ApplicationRecord
     has_many :set_lists
 
     def full_location
-        "#{self.street_address}"\
-        "#{self.secondary_address}" \
-        "#{self.city}, #{self.state} #{self.zip}"
+        return 'No Address Available' if self.street_address.blank?
+        parts = [self.street_address, self.secondary_address, self.city, self.state, self.zip]
+        parts.reject!(&:blank?)
+        parts.join('<br/>').html_safe
+
+        # "#{self.street_address}"\
+        # "#{self.secondary_address}"\
+        # "#{self.city}, #{self.state} #{self.zip}"
     end
 
     def formatted_start_time
@@ -14,5 +19,9 @@ class Gig < ApplicationRecord
 
     def formatted_end_time
         self.end_time.strftime("%B %d, %Y, %I:%M%P")
+    end
+
+    def formatted_date_title
+        self.start_time.strftime("%B %d, %Y")
     end
 end
