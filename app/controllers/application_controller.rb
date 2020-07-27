2aa5d232535dd12f5136657639f0b8abfd3b949c
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-07-26 23:46:49 -0600

    helper methods and sessions login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-   def require_login
-    !!session[:user_id]
-   end
+    helper_method :current_user
+    helper_method :require_login
+    
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+    
+    def require_login
+        !!current_user
+    end
 end
